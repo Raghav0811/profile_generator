c6fcde6df1020b3c54cb792815cfa398ba0eead5
vagrant <raghavbpdshrivastav@gmail.com>
2020-05-06 12:58:50 +0000

    added more info to survey.js

--- survey.js
+++ survey.js
@@ -7,10 +7,12 @@ const rl = readline.createInterface({
 
 rl.question('Whats your name? ', (answer1) => {
   rl.question("How old are you? ", (answer2) => {
-    rl.question("What's your favorite food? ", (answer3) => { 
-      rl.question("Which is your favorite sport? ", (answer4) => { 
-        console.log(`${answer1} is ${answer2} years old and their favorite thing to eat is ${answer3}. Their favorite sport is ${answer4}.`);
-      rl.close();
+    rl.question("What's an activity you like doing? ", (answer3) => {
+      rl.question("What's your favorite food? ", (answer4) => { 
+       rl.question("Which is your favorite sport? ", (answer5) => { 
+        console.log(`${answer1} is ${answer2} years old and their favorite activity is ${answer3}. Their favourite food is ${answer4} and favourite sport is ${answer5}.`);
+        rl.close();
+        });
       });
     });
   });
